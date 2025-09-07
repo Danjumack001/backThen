cebebe65821da22a2c5e42774c84ea5227adc3ed
Danjumack001 <danjumazicc@gmail.com>
2025-09-07 15:32:22 +0100

    second commit - timeline project

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const mongoose = require('mongoose')
-const connect = mongoose.connect("mongodb+srv://danjumaisaac:rDeExusVH1F0TGRk@shopa.52iy6.mongodb.net/timeline?retryWrites=true&w=majority&appName=Shopa")
+
+const connect = mongoose.connect(process.env.MONGO_URI)
 
 
 connect.then(()=>{
